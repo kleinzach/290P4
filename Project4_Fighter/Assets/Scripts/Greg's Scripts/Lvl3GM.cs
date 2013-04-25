ab0a71cd2d623f6d274d6700d27eda0efaf7cacb
Gregory Ziegan <grz5@case.edu>
2013-04-25 03:35:15 -0400

    Minor Changes to levels after branch

--- Project4_Fighter/Assets/Scripts/Greg's Scripts/Lvl3GM.cs
+++ Project4_Fighter/Assets/Scripts/Greg's Scripts/Lvl3GM.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class Lvl3GM : GameMaster {
 
-	// Use this for initialization
-	void Start () {
+	private GameObject newPowerUp;
+	public float spawnRate = 5f;
 	
+	override public void Start () {
+		base.Start();
+		
+		StartCoroutine(spawnPowerUps(getRandomPowerUp()));
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	IEnumerator spawnPowerUps(GameObject powerUp) {
+		newPowerUp = Instantiate(powerUp, new Vector3(Random.Range(-7, 7),Random.Range(-2, 8), 0), Quaternion.identity) as GameObject;
+		yield return new WaitForSeconds(spawnRate);
+		StartCoroutine(spawnPowerUps(getRandomPowerUp()));
+	}
 	
+	GameObject getRandomPowerUp() {
+		int randomInt = Random.Range(0,4);
+		switch(randomInt) {
+			case 0:
+				return LEPUPrefab; // Random Power Up
+				break;
+			case 1: 
+				return bombPUPrefab; // Bomb Power Up 
+				break;
+			case 2:
+				return healPrefab; // Heal Power Up
+				break;
+			case 3:
+				return increaseSpeedPUPrefab; // Increase Speed 
+				break;
+		}
+		return null;
 	}
 }
